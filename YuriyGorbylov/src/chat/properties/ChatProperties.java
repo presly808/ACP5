aeeb1a3bda64c8fb498a535a89406dee841b1a59
GorbylovYuriy <yuriygorbylov@gmail.com>
2015-02-10 17:26:26 +0200

    chat v2.4

---
+++ YuriyGorbylov/src/chat/properties/ChatProperties.java
@@ -0,0 +1,7 @@
+package chat.properties;
+
+/**
+ * Created by yuriy.gorbylev on 10.02.2015.
+ */
+public class ChatProperties {
+}
---
+++ YuriyGorbylov/src/chat/server/ServerPacket.java
@@ -0,0 +1,7 @@
+package chat.server;
+
+/**
+ * Created by yuriy.gorbylev on 10.02.2015.
+ */
+public class ServerPacket {
+}
